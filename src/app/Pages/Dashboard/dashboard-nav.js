0682501fed7e6ca1f672aff498222edd3361ac15
Francisco Arleo <francisco.arleo@globant.com>
2019-02-25 16:29:30 -0500

    Fixing Link error on nav

--- src/app/Pages/Dashboard/dashboard-nav.js
+++ src/app/Pages/Dashboard/dashboard-nav.js
@@ -4,17 +4,19 @@ import Link from 'react-router-dom';
 class DashboardNav extends React.Component {
     render() {
         return(
-            <ul>
-                <li>
-                    Dashboard
-                </li>
-                <li>
-                    Speakers
-                </li>
-                <li>
-                    Events
-                </li>
-            </ul>
+            <div>    
+                <ul>
+                    <li>
+                            Dashboard
+                    </li>
+                    <li>
+                        Speakers
+                    </li>
+                    <li>
+                        Events
+                    </li>
+                </ul>
+            </div>
         )
     }
 }
